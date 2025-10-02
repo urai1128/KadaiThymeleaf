4240271f67bb525ffcb5f19bad541471fd4c261b
urai1128 <souma112877@gmail.com>
2025-10-03 07:23:14 +0900

    再コミット

---
+++ src/main/java/com/example/kadaithymeleaf/KadaiThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.example.kadaithymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KadaiThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KadaiThymeleafApplication.class, args);
+    }
+}
